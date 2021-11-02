7c2ff3e6426352f9d6e2bc7108cb5f9eb1d923b5
Olaf Monien <olaf@monien.net>
2021-11-02 10:48:16 +0100

    C2A0 non-breaking spaces fixed.

--- SieveOfEratosthenes.pas
+++ SieveOfEratosthenes.pas
@@ -10,93 +10,93 @@ unit SieveOfEratosthenes;
 interface
 
 uses
-  system.classes, system.generics.collections;
+  system.classes, system.generics.collections;
 
 const
-  BlockSize = 64;
+  BlockSize = 64;
 
 type
-  ArrayOfUInt32 = array of uint32;
-  TSieveOfEratosthenes = Class
-  private
-    class function NumberOfBitsSet: uint32;
-  class var
-    FMaxValue: uint32;
-    FMaxValueISqrt: uint32; // Trunc(Sqrt(MaxValue))
-    FMaxBlockIndex: uint32; // floor(MaxValue/BlockSize)
-    FValueBitField: Array of UInt64; { [0 .. MaxBlockIndex] }
-    class procedure SetMaxValue(const Value: uint32); static;
-    class function GetNextPrime(APreviousPrime: uint32): uint32;
-    class constructor ClassCreate;
-    class procedure GetPrimeMask; static; // 3 and up
-  public
-    class function GetPrimes(const AMaxValue: uint32): ArrayOfUInt32; static;
-  End;
+  ArrayOfUInt32 = array of uint32;
+  TSieveOfEratosthenes = Class
+  private
+   class function NumberOfBitsSet: uint32;
+  class var
+   FMaxValue: uint32;
+   FMaxValueISqrt: uint32; // Trunc(Sqrt(MaxValue))
+   FMaxBlockIndex: uint32; // floor(MaxValue/BlockSize)
+   FValueBitField: Array of UInt64; { [0 .. MaxBlockIndex] }
+   class procedure SetMaxValue(const Value: uint32); static;
+   class function GetNextPrime(APreviousPrime: uint32): uint32;
+   class constructor ClassCreate;
+   class procedure GetPrimeMask; static; // 3 and up
+  public
+   class function GetPrimes(const AMaxValue: uint32): ArrayOfUInt32; static;
+  End;
 
 implementation
 
 uses
-  system.Math;
+  system.Math;
 
 { TSieveOfErastothenos }
 // https://stackoverflow.com/questions/2709430/count-number-of-bits-in-a-64-bit-long-big-integer
 class function TSieveOfEratosthenes.NumberOfBitsSet: uint32;
 var
-  i: integer;
-  LBitFieldCount: UInt64;
+  i: integer;
+  LBitFieldCount: UInt64;
 begin
-  result := 0;
-  for i := 0 to FMaxBlockIndex do
-  begin
-    LBitFieldCount := FValueBitField[i] - ((FValueBitField[i] shr 1) and
-      UInt64($5555555555555555));
+  result := 0;
+  for i := 0 to FMaxBlockIndex do
+  begin
+   LBitFieldCount := FValueBitField[i] - ((FValueBitField[i] shr 1) and
+    UInt64($5555555555555555));
 
-    LBitFieldCount := (LBitFieldCount and UInt64($3333333333333333)) +
-      ((LBitFieldCount shr 2) and UInt64($3333333333333333));
+   LBitFieldCount := (LBitFieldCount and UInt64($3333333333333333)) +
+    ((LBitFieldCount shr 2) and UInt64($3333333333333333));
 
-    LBitFieldCount :=
-      byte((((LBitFieldCount + (LBitFieldCount shr 4)) and
-      UInt64($F0F0F0F0F0F0F0F)) * UInt64($101010101010101)) shr 56);
+   LBitFieldCount :=
+    byte((((LBitFieldCount + (LBitFieldCount shr 4)) and
+    UInt64($F0F0F0F0F0F0F0F)) * UInt64($101010101010101)) shr 56);
 
-    result := result + LBitFieldCount;
-  end;
+   result := result + LBitFieldCount;
+  end;
 end;
 
 class procedure TSieveOfEratosthenes.SetMaxValue(const Value: uint32);
 begin
-  FMaxValue := Value;
-  FMaxValueISqrt := Trunc(Sqrt(FMaxValue));
-  FMaxBlockIndex := floor(FMaxValue / BlockSize);
-  FValueBitField := nil; // will make ensure zeros when we call setlength below
-  SetLength(FValueBitField, FMaxBlockIndex + 1);
+  FMaxValue := Value;
+  FMaxValueISqrt := Trunc(Sqrt(FMaxValue));
+  FMaxBlockIndex := floor(FMaxValue / BlockSize);
+  FValueBitField := nil; // will make ensure zeros when we call setlength below
+  SetLength(FValueBitField, FMaxBlockIndex + 1);
 end;
 
 class constructor TSieveOfEratosthenes.ClassCreate;
 begin
-  SetMaxValue(1000000);
+  SetMaxValue(1000000);
 end;
 
 // PRE: APreviousPrime is prime and > 2
 // POST: return value is the successor prime of the input
 class function TSieveOfEratosthenes.GetNextPrime(APreviousPrime
-  : uint32): uint32;
+  : uint32): uint32;
 var
-  LBlock: UInt64;
-  LBlockIndex: UInt64;
-  LBlockBitIndex: UInt64;
+  LBlock: UInt64;
+  LBlockIndex: UInt64;
+  LBlockBitIndex: UInt64;
 begin
-  DivMod(APreviousPrime + 2, BlockSize, LBlockIndex, LBlockBitIndex);
-  repeat
-    LBlock := FValueBitField[LBlockIndex] shr LBlockBitIndex;
-    if LBlockBitIndex >= BlockSize then
-    begin
-      LBlockBitIndex := 0;
-      inc(LBlockIndex);
-    end
-    else
-      inc(LBlockBitIndex);
-  until (LBlock and 1 = 0);
-  result := LBlockIndex * BlockSize + LBlockBitIndex - 1;
+  DivMod(APreviousPrime + 2, BlockSize, LBlockIndex, LBlockBitIndex);
+  repeat
+   LBlock := FValueBitField[LBlockIndex] shr LBlockBitIndex;
+   if LBlockBitIndex >= BlockSize then
+   begin
+    LBlockBitIndex := 0;
+    inc(LBlockIndex);
+   end
+   else
+    inc(LBlockBitIndex);
+  until (LBlock and 1 = 0);
+  result := LBlockIndex * BlockSize + LBlockBitIndex - 1;
 end;
 
 // class procedure TSieveOfEratosthenes.GetPrimeMask;
@@ -105,12 +105,12 @@ end;
 //		to index. Zero based index directly correlates to values
 class procedure TSieveOfEratosthenes.GetPrimeMask;
 var
-  i: uint32;
-  LPrime: uint32;
-  LMultiple: uint32;
-  LNonPrime: UInt64;
-  LBlockIndex: UInt64;
-  LBlockBitIndex: UInt64;
+  i: uint32;
+  LPrime: uint32;
+  LMultiple: uint32;
+  LNonPrime: UInt64;
+  LBlockIndex: UInt64;
+  LBlockBitIndex: UInt64;
 begin
   //we could set the initial state of the blocks. since 
   // multiples of 2 are really trivial bit patterns 
@@ -123,45 +123,45 @@ begin
     FValueBitField[i] := UInt64($5555555555555555);
   end;
   
-  LPrime := 3;
-  repeat
-    LMultiple := LPrime;
-    LNonPrime := LPrime * LMultiple;
-    while LNonPrime <= FMaxValue do
-    begin
-      DivMod(LNonPrime, BlockSize, LBlockIndex, LBlockBitIndex);
-  
-      FValueBitField[LBlockIndex] := FValueBitField[LBlockIndex] or
-        (UInt64(1) shl LBlockBitIndex);
+  LPrime := 3;
+  repeat
+   LMultiple := LPrime;
+   LNonPrime := LPrime * LMultiple;
+   while LNonPrime <= FMaxValue do
+   begin
+    DivMod(LNonPrime, BlockSize, LBlockIndex, LBlockBitIndex);
+ 
+     FValueBitField[LBlockIndex] := FValueBitField[LBlockIndex] or
+     (UInt64(1) shl LBlockBitIndex);
 
-      inc(LMultiple);
+    inc(LMultiple);
 
-      LNonPrime := LPrime * LMultiple;
-    end;
-    LPrime := GetNextPrime(LPrime);
-  until (LPrime >= FMaxValueISqrt);
+    LNonPrime := LPrime * LMultiple;
+   end;
+   LPrime := GetNextPrime(LPrime);
+  until (LPrime >= FMaxValueISqrt);
 end;
 
 class function TSieveOfEratosthenes.GetPrimes(const AMaxValue: uint32)
-  : ArrayOfUInt32;
+  : ArrayOfUInt32;
 var
-  LPrime: Uint32;
-  i: integer;
+  LPrime: Uint32;
+  i: integer;
 begin
-  SetMaxValue(AMaxValue);
-  GetPrimeMask;
-  SetLength(result, FMaxValue - NumberOfBitsSet);
-  result[0] := 2;
-  result[1] := 3;
-  LPrime := 3;
- 
+  SetMaxValue(AMaxValue);
+  GetPrimeMask;
+  SetLength(result, FMaxValue - NumberOfBitsSet);
+  result[0] := 2;
+  result[1] := 3;
+  LPrime := 3;
+ 
   i := 2;
-  while LPrime <= FMaxValue do
-  begin
-    LPrime := GetNextPrime(LPrime);
-    result[i] := LPrime;
-    inc(i);
-  end;
+  while LPrime <= FMaxValue do
+  begin
+   LPrime := GetNextPrime(LPrime);
+   result[i] := LPrime;
+   inc(i);
+  end;
 end;
 end.
 
